5d6734977ad7977d561550d3cc0f66dfc83fb251
Kashish <kashish.2024it1185@kiet.edu>
2023-07-20 08:32:03 +0530

    commit

---
+++ Day 12/ind Median from Data Stream.py
@@ -0,0 +1,14 @@
+from sortedcontainers import SortedList
+class MedianFinder:
+
+    def __init__(self):
+        self.data = SortedList()
+
+    def addNum(self, num: int) -> None:
+        self.data.add(num)
+
+    def findMedian(self) -> float:
+        lnth = len(self.data)
+        return self.data[lnth//2] if lnth%2 else (self.data[lnth//2] + self.data[lnth//2 - 1])/2
+
+
