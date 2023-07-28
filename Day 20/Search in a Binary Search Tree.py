899b6571d9cdb838d54c57946517f2944ec0026b
Kashish <kashish.2024it1185@kiet.edu>
2023-07-28 09:01:45 +0530

    commit

---
+++ Day 20/Search in a Binary Search Tree.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def searchBST(self, root, val):
+        current_node = root
+        while current_node != None:
+            if current_node.val == val:
+                return current_node
+            elif current_node.val < val:
+                current_node = current_node.right
+            else:
+                current_node = current_node.left
+        return None
